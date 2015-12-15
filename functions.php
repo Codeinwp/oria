540ac25bb2dc8285930f270d060aeb9ab98e9ecd
abaicus <baicusandrei@me.com>
2015-12-15 17:23:45 +0200

    !!!Fixed textdomain of tgm

--- functions.php
+++ functions.php
@@ -288,7 +288,7 @@ require_once get_template_directory() . '/class-tgm-plugin-activation.php';
 /**
  * TGMPA register
  */
-function chef_register_required_plugins() {
+function oria_register_required_plugins() {
 		$plugins = array(
 			array(
 				'name'      => 'WP Product Reviews',
@@ -317,23 +317,23 @@ function chef_register_required_plugins() {
         'is_automatic' => false,
         'message'      => '',
         'strings'      => array(
-            'page_title'                      => esc_html__( 'Install Required Plugins', 'chef' ),
-            'menu_title'                      => esc_html__( 'Install Plugins', 'chef' ),
-            'installing'                      => esc_html__( 'Installing Plugin: %s', 'chef' ),
-            'oops'                            => esc_html__( 'Something went wrong with the plugin API.', 'chef' ),
-            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'chef' ),
-            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'chef' ),
-            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'chef' ),
-            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'chef' ),
-            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'chef' ),
-            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'chef' ),
-            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'chef' ),
-            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'chef' ),
-            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'chef' ),
-            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'chef' ),
-            'return'                          => esc_html__( 'Return to Required Plugins Installer', 'chef' ),
-            'plugin_activated'                => esc_html__( 'Plugin activated successfully.', 'chef' ),
-            'complete'                        => esc_html__( 'All plugins installed and activated successfully. %s', 'chef' ),
+            'page_title'                      => esc_html__( 'Install Required Plugins', 'oria' ),
+            'menu_title'                      => esc_html__( 'Install Plugins', 'oria' ),
+            'installing'                      => esc_html__( 'Installing Plugin: %s', 'oria' ),
+            'oops'                            => esc_html__( 'Something went wrong with the plugin API.', 'oria' ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'oria' ),
+            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'oria' ),
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'oria' ),
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'oria' ),
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'oria' ),
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'oria' ),
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'oria' ),
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'oria' ),
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'oria' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'oria' ),
+            'return'                          => esc_html__( 'Return to Required Plugins Installer', 'oria' ),
+            'plugin_activated'                => esc_html__( 'Plugin activated successfully.', 'oria' ),
+            'complete'                        => esc_html__( 'All plugins installed and activated successfully. %s', 'oria' ),
             'nag_type'                        => 'updated'
         )
     );
@@ -341,4 +341,4 @@ function chef_register_required_plugins() {
 	tgmpa( $plugins, $config );
 
 }
-add_action( 'tgmpa_register', 'chef_register_required_plugins' );
+add_action( 'tgmpa_register', 'oria_register_required_plugins' );
