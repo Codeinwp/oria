cfc82a151a01a732ad2b3addf31cc37167ab8e05
Bego Mario Garde <begomario.garde@gmx.de>
2016-06-17 15:13:42 +0200

    Fix for #9
    Fixes missing path for `get_template_part()`.

--- page-templates/page_fullwidth.php
+++ page-templates/page_fullwidth.php
@@ -14,7 +14,7 @@ Template Name: Full width
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 				<?php
 
